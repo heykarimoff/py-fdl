eaa6ab1be9b70249da787e3f890ef1fa89f53d60
Mukhammad Karimov <karimov8887@gmail.com>
2018-05-06 16:20:22 +0500

    [tests] add initial pytest

--- tests/test_dynamic_links.py
+++ tests/test_dynamic_links.py
@@ -1,5 +1,12 @@
 import unittest
 
+from firebase_dynamic_links.dynamic_links import DynamicLink
 
-def test_something():
-    pass
+
+def test_dynamic_link_app_code():
+    app_code = 'my_app_code'
+    dynamic_link = DynamicLink(app_code=app_code, link='https://www.google.com')
+
+    link = dynamic_link.get_long_link()
+
+    assert app_code in link

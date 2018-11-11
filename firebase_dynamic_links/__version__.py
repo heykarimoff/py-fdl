bed465e7d1706095af2f60948f3efdd0a9051875
Mukhammad Karimov <brown@mysuperdispatch.com>
2018-11-11 22:36:16 +0500

    [firebase_dynamic_links] version 1.1.2 (beta release)

--- firebase_dynamic_links/__version__.py
+++ firebase_dynamic_links/__version__.py
@@ -1,8 +1,8 @@
 __title__ = 'py-fdl'
 __description__ = 'Python client for Firebase Dynamic Links'
 __url__ = 'https://github.com/heykarimoff/py-fdl'
-__version__ = '1.1.0'
-__build__ = 0x010002
+__version__ = '1.1.2'
+__build__ = 0x010102
 __author__ = 'Mukhammad Karimov'
 __author_email__ = 'hey@karimoff.me'
 __license__ = 'MIT'

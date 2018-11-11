dffc8a1eef03e77a57d66311b4adbf40f23c2334
Mukhammad Karimov <brown@mysuperdispatch.com>
2018-11-11 18:16:38 +0500

    [firebase_dynamic_links] added __version__ file for package information

---
+++ firebase_dynamic_links/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'firebase_dynamic_links'
+__description__ = 'Python client for Firebase Dynamic Links'
+__url__ = 'https://github.com/heykarimoff/py-fdl'
+__version__ = '1.0.2'
+__build__ = 0x010002
+__author__ = 'Mukhammad Karimov'
+__author_email__ = 'hey@karimoff.me'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Mukhammad Karimov'
+__keywords__ = 'firebase dynamic links super links'
